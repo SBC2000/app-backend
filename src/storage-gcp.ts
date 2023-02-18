2c4f8d5d348813885effd07cc07e64974e539123
Vincent van der Weele <vincentvanderweele@hotmail.com>
2023-02-18 15:38:15 +0200

    Add initial Cloud storage implementation

---
+++ src/storage-gcp.ts
@@ -0,0 +1,93 @@
+import {
+  Bucket,
+  File,
+  GetFilesOptions,
+  Storage as StorageSdk,
+} from "@google-cloud/storage";
+
+import { Logger } from "./logger";
+import { StorageBase } from "./storage";
+
+export class GcpStorage extends StorageBase {
+  private bucket: Bucket;
+  private logger: Logger;
+
+  public constructor(
+    storageSdk: StorageSdk,
+    bucketName: string,
+    logger: Logger
+  ) {
+    super();
+
+    this.bucket = storageSdk.bucket(bucketName);
+    this.logger = logger;
+  }
+
+  public async createFolder(folder: string): Promise<void> {
+    const filePath = `${folder}/`;
+
+    this.logger.debug(`Create folder: ${filePath}`);
+
+    await this.bucket
+      .file(filePath)
+      .save("", { contentType: "application/x-www-form-urlencoded" });
+  }
+
+  protected async listDirectories(prefix?: string): Promise<string[]> {
+    const objects = await this.listObjects(prefix, "/");
+    return objects.map((object) => object.name).map((x) => x.split("/")[0]);
+  }
+
+  protected async listFiles(prefix?: string): Promise<string[]> {
+    const objects = await this.listObjects(prefix);
+    return objects.map((object) => object.name);
+  }
+
+  protected async listObjects(
+    prefix?: string,
+    delimiter?: string
+  ): Promise<File[]> {
+    const result: File[] = [];
+
+    let query: GetFilesOptions = {
+      delimiter,
+      includeTrailingDelimiter: !!delimiter,
+      prefix,
+      autoPaginate: false,
+      maxResults: 20,
+    };
+
+    while (query) {
+      this.logger.debug(`Get files: ${JSON.stringify(query, null, 2)}`);
+
+      const [files, nextQuery] = await this.bucket.getFiles(query);
+
+      result.push(...files);
+      query = {
+        ...query,
+        ...nextQuery,
+      };
+    }
+
+    return result;
+  }
+
+  protected async readFileContents(
+    filePath: string
+  ): Promise<Buffer | undefined> {
+    const [buffer] = await this.bucket.file(filePath).download();
+
+    return buffer;
+  }
+
+  protected async writeFileContents(
+    filePath: string,
+    contents: string
+  ): Promise<void> {
+    this.logger.debug(`Create file: ${filePath}`);
+
+    await this.bucket
+      .file(filePath)
+      .save(contents, { contentType: "application/json" });
+  }
+}
